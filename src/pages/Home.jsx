130090989fb42949340069d5690b8de1bad27ae4
MSA <crystalbluetech@gmail.com>
2023-08-11 15:30:53 +0100

    fix searchTerm to enable filtering of countries on search

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -20,5 +20,26 @@ const Home = () => {
     (country) => country.name.toLowerCase().includes(searchTerm.toLowerCase()),
   );
 
-  
+  return (
+    <>
+      <div className="App">
+        <Header previous=" " />
+        <Hero title="" searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        <div className="countries">
+          {filteredCountries.map((country, index) => (
+            <Country
+              key={country.name}
+              name={country.name}
+              vector={country.map}
+              cities={country.cities}
+              airquality={country.airquality}
+              isOdd={index % 2 !== 0}
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
 export default Home;
